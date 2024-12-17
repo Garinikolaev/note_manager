bedbc9dd0ab12494bc8872b773af2eb006092f07
Garinikolaev <gari.nikolaev@yandex.ru>
2024-12-17 22:20:31 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,14 @@
+username = "имя пользователя"
+print("имя пользователя:", username)
+title = "заголовок заметки"
+print("заголовок заметки:", title)
+content = "описание заметки"
+print("описание заметки:", content)
+status = "статус заметки"
+print("статус заметки:", status)
+created_date = "10-11-2024"
+temp_created_date = "10.11"
+print("дата создания заметки:", temp_created_date)
+issue_date = ("10-12-2024")
+temp_issue_date = "10.12"
+print("дата истечения заметки:", temp_issue_date)
