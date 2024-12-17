3614e2b0d2df2ab982bd0b8258f2f60d3b23d8fe
Garinikolaev <gari.nikolaev@yandex.ru>
2024-12-17 22:00:16 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "имя пользователя"
+print("имя пользователя:", username)
+title = "заголовок заметки"
+print("заголовок заметки:", title)
+content = "описание заметки"
+print("описание заметки:", content)
+status = "статус заметки"
+print("статус заметки:", status)
+created_date = "10-11-2024"
+print("дата создания заметки:", created_date)
+issue_date = ("10-12-2024")
+print("дата истечения заметки:", issue_date)
