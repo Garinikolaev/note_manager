0005d28d7970166eba10d54557a4be5946e98d18
Garinikolaev <gari.nikolaev@yandex.ru>
2024-12-30 15:29:48 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -6,13 +6,7 @@ content = input('Напишите описание заметки: ')
 status = input('Проставьте статус заметки: ')
 created_date = input('Введите дату создания заметки: ')
 issue_date = input('Введите дату изменения заметки: ')
-note = [username,
-        [title1,
-        title2],
-        content,
-        status,
-        created_date,
-        issue_date]
+note = [username, [title1, title2], content, status, created_date, issue_date]
 print('Имя пользователя: ', username)
 print('Заголовок заметки 1: ', title1)
 print('Заголовок заметки 2: ', title2)
@@ -20,3 +14,4 @@ print('Описание заметки: ', content)
 print('Статус заметки:', status)
 print('Дата создания заметки: ', created_date)
 print('Дата истечения заметки: ', issue_date)
+print(note)
